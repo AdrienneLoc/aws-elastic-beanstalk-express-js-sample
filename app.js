f250a219d34bf40ecda0bb7f82cdbc37dfee8b19
Adrienne Loc <al738931@sju.edu>
2023-10-27 19:05:10 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Continuous Delivery Pipeline Tutorial'));
+app.get('/', (req, res) => res.send('Continuous Delivery Pipeline Tutorial. Second changed message.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
